858e2319b63b600fc319c5b50a34a3610a76bea9
Puneet Pugalia <pugaliapuneet@gmail.com>
2023-04-30 12:30:14 +0530

    Initialize git repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# Best sellers in books
+
+
+![Books](https://user-images.githubusercontent.com/60655590/227194640-f055efdf-51cb-46b7-b36b-1afe3596dab9.png)
+
+## Visit website
+[Visit website](https://react-amazon-bestsellers-books-dy.netlify.app/)
+
+## Built With
+
+* HTML
+* CSS
+* React
+
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import { books } from './data/books';
+import Book from './components/Book';
+
+const BookList = () => {
+  return (
+    <div className='main-body'>
+      <h1 className='main-title'>
+        Best Sellers <span>in Books</span>
+      </h1>
+      <h4 className='date'>April 2023</h4>
+      <article className='book-list'>
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </article>
+      <section className='bottom'>
+        <h5 className='bottom-title'>
+          Amazon-bestsellers-books using <span>React</span>
+        </h5>
+        <h5 className='bottom-dy'>
+          <a href='https://www.linkedin.com/in/pranaypise/' target='_blank' rel='noreferrer'>
+            Linkedin
+          </a>
+        </h5>
+      </section>
+    </div>
+  );
+};
+
+export default BookList;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList />);
